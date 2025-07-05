951302de90297b4687410fe055bad791809645de
Namrata Singh <144470271+namrata-singh-student@users.noreply.github.com>
2025-07-05 22:41:29 +0530

    commit7

--- pgip-backend/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Sample route
-router.get("/", (req, res) => {
-  res.send("Hello from router!");
-});
-
-module.exports = router;
